ab3cc3f19a2a22286371f558768b26b671ad836e
Chia-Rong Chang <ccdrumer@gmail.com>
2015-05-10 13:56:59 +0800

    evernote example

--- js/reqInit.js
+++ js/reqInit.js
@@ -6,11 +6,15 @@ requirejs.config({
 		bootstrap: 	'bootstrap.min',
 		jquery: 	'jquery-1.9.1.min',
 		masonry: 	'masonry.pkgd.min',
-		evernote: 	'evernote-sdk-minified'
+		evernote: 	'evernote-sdk-minified',
+		jsOAuth:	'jsOAuth-1.3.7.min'
 	},
 	shim: {
 		bootstrap: {
 			deps: ["jquery"]
+		},
+		evernote: {
+			deps: ["jsOAuth"]
 		}
 	}
 });
